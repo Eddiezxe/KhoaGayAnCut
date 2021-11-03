be5b50e3fabc35343ec3a64f85924dbf68e6c694
Eddiezxe <tuanmai1509r@gmail.com>
2021-11-03 21:59:29 +0700

    display key matrix key on form

--- Form1.cs
+++ Form1.cs
@@ -16,7 +16,14 @@ namespace KhoaGayAnCut
         {
             InitializeComponent();
         }
-
+        void displayKeyMatrix(string[,] key) // đừng đụng vào
+        {
+            key00.Text = key[0, 0]; key01.Text = key[0, 1]; key02.Text = key[0, 2]; key03.Text = key[0, 3]; key04.Text = key[0, 4];
+            key10.Text = key[1, 0]; key11.Text = key[1, 1]; key12.Text = key[1, 2]; key13.Text = key[1, 3]; key14.Text = key[1, 4];
+            key20.Text = key[2, 0]; key21.Text = key[2, 1]; key22.Text = key[2, 2]; key23.Text = key[2, 3]; key24.Text = key[2, 4];
+            key30.Text = key[3, 0]; key31.Text = key[3, 1]; key32.Text = key[3, 2]; key33.Text = key[3, 3]; key34.Text = key[3, 4];
+            key40.Text = key[4, 0]; key41.Text = key[4, 1]; key42.Text = key[4, 2]; key43.Text = key[4, 3]; key44.Text = key[4, 4];
+        }
         void separateMsg(string msg, string[] separatedMsg)
         {
             string newMsg = msg;
@@ -33,15 +40,12 @@ namespace KhoaGayAnCut
 
 
         }
-        void takeInput()
+        void takeMesage()
         {
             textBoxAnswer.Clear();
             string[] PairCharacter = new string[30];
             string msg = "";
             msg = textBoxMsg.Text.Trim().ToUpper();
-
-            string keyWord = "";
-            keyWord = textBoxKey.Text.Trim().ToUpper();
 
             separateMsg(msg, PairCharacter);
 
@@ -51,7 +55,22 @@ namespace KhoaGayAnCut
 
         private void buttonEncrypt_Click(object sender, EventArgs e)
         {
-            takeInput();
+            string[,] keymatrix = new string[5, 5];
+            for(int i = 0; i<5; i++)
+            {
+                for(int j = 0; j < 5; j++)
+                {
+                    keymatrix[i, j] = "B";
+                }    
+            }
+            displayKeyMatrix(keymatrix);
+            takeMesage();
         }
     }
-}
+}       /*
+            key00.Text = key[0][0]; key01.Text = key[0][1]; key02.Text = key[0][2]; key03.Text = key[0][3]; key04.Text = key[0][4];
+            key10.Text = key[1][0]; key11.Text = key[1][1]; key12.Text = key[1][2]; key13.Text = key[1][3]; key14.Text = key[1][4];
+            key20.Text = key[2][0]; key21.Text = key[2][1]; key22.Text = key[2][2]; key23.Text = key[2][3]; key24.Text = key[2][4];
+            key30.Text = key[3][0]; key31.Text = key[3][1]; key32.Text = key[3][2]; key33.Text = key[3][3]; key34.Text = key[3][4];
+            key40.Text = key[4][0]; key41.Text = key[4][1]; key42.Text = key[4][2]; key43.Text = key[4][3]; key44.Text = key[4][4];
+         */
